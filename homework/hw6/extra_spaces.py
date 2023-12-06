39dfdaa89ca29225bd865fa043e786e4d34825f0
xeustain <145146886+xeustain@users.noreply.github.com>
2023-12-06 16:22:04 +0300

    Add files via upload

---
+++ homework/hw6/extra_spaces.py
@@ -0,0 +1,7 @@
+primary_text = input('Введите любой текст, при вводе текста вы можете допускать дублирование пробелов\n>>>')
+
+primary_text =' '.join(primary_text.split())
+
+print(primary_text)
+
+input()
